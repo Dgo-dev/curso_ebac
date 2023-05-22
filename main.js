8f48e27a5dd29f76d5176d4bb53158c20222a24b
Diego <dssilva0710@hotmail.com>
2023-05-22 20:23:55 -0300

    corrigindo dúvidas de javascript

--- main.js
+++ main.js
@@ -1,22 +1,18 @@
 $(document).ready(function(){
-
-    $('body button').click(function(){
-
-    })
-
     $('form').on('submit', function(e) {
         e.preventDefault();
 
         const addTask = $('#task').val();
-        const novaTask = $(`<li style="display:block;">${addTask}</li>`).appendTo('ul').click();
-        console.log($('li').addClass('add-lista'));
-        $('#task').val('');
-        
-        $('ul').on('click', 'li', function(e){
-            $(novaTask).addClass('riscado')
+        const novaTask = $(`<li style="display:block;">${addTask}</li>`);
+        novaTask.on('click', function() {
+           $('#task').val('');
+           $(this).toggleClass('riscado');
+
         });
 
-       
+        novaTask.appendTo('ul');    
+        $('#task').val('');
+
     })
 
 })
